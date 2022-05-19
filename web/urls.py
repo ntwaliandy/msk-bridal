3f75ec236158aee887f775b9558f073d12cfadc7
ntwaliandy <ntwaliandy90@gmail.com>
2022-05-19 14:29:32 +0300

    initial commit"

--- web/urls.py
+++ web/urls.py
@@ -1,13 +1,12 @@
 from os import name
-from django.urls import path
-from django.conf.urls import url
+from django.urls import path, re_path
 from . import views
 app_name = 'web'
 urlpatterns = [
 	path('', views.index, name='index'),
-	url(r'^(?P<product_id>[0-9]+)/$', views.detail, name='detail'),
-	url(r'^category/(?P<category_id>[0-9]+)/$', views.shop, name='shop'),
-	url(r'^(?P<product_id>[0-9]+)/checkout/$', views.checkout, name='checkout'),
+	re_path(r'^(?P<product_id>[0-9]+)/$', views.detail, name='detail'),
+	re_path(r'^category/(?P<category_id>[0-9]+)/$', views.shop, name='shop'),
+	re_path(r'^(?P<product_id>[0-9]+)/checkout/$', views.checkout, name='checkout'),
 	path('contact/', views.contact, name='contact'),
 	path('about/', views.about, name='about')
 
